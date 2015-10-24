d72d93cf35f38a899535c4200d0aa04d777ca83f
Gorova <gorova2006gmail.com>
2015-10-24 19:17:34 +0300

    mapping

---
+++ ConsoleApplication2/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product{Name = "coffe", Price = 40, Id = 100};
+            Mapper.CreateMap<Product, ProductViewModel>();
+            var productViewModel = Mapper.Map<Product, ProductViewModel>(product);
+            Console.WriteLine(productViewModel.Name);
+        }
+    }
+}
